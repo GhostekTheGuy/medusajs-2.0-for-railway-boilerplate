dbd610eb95247b743e2335d2819599effa9c087b
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2024-10-05 17:09:55 +0200

    futer

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -18,7 +18,11 @@ export default async function Footer() {
               href="/"
               className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
             >
-              Lori Store
+              <img 
+                src="https://cdn.discordapp.com/attachments/1285889428929253386/1285991184828727387/lori_low.png?ex=670208df&is=6700b75f&hm=04ba1e397d5e082a8a6d544dec72d00457a6120151f3371690ccfdd57d1bf35c&" 
+                alt="Lori Store Logo" 
+                className="h-auto w-auto" // Możesz dostosować rozmiar obrazka
+              />
             </LocalizedClientLink>
           </div>
           <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
