b88e04703c476e62870378db64512df385a49dc3
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2024-10-12 14:21:15 +0200

    new2

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,12 +1,13 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 import { BoltSolid } from "@medusajs/icons"
+import imgHERO from "./images/kanapa.png"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
+        <div>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-white font-bold"
@@ -19,18 +20,19 @@ const Hero = () => {
           >
             Get ready and buy preorder.
           </Heading>
-        </span>
+        </div>
         <a
           href="/new-arrivals"
-          target="_blank"        >
-          <Button variant="secondary">
+          target="_blank"
+        >
+          <Button variant="secondary" aria-label="Discover New Arrivals">
             Discover New Arrivals
             <BoltSolid />
           </Button>
         </a>
       </div>
       <div className="absolute inset-0 w-full h-full">
-        <img src="./images/kanapa.png" alt="Kanapa" />
+        <img src={imgHERO} alt="Kanapa" loading="lazy" />
       </div>
     </div>
   )
